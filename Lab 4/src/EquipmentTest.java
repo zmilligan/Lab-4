0ae8ac1245ab67f22408b1c3d3a9426d50518dbc
Zandi Milligan <zmilligan@ou.edu>
2019-02-08 15:49:52 -0600

    Finished getter test methods

--- Lab 4/src/EquipmentTest.java
+++ Lab 4/src/EquipmentTest.java
@@ -6,7 +6,7 @@
  */
 public class EquipmentTest
 {
-	private String info = "Thor's Hammer/1,10,99999999,Cannot be wielded by those unworthy";
+	private final String INFO = "Thor's Hammer/1,10,99999999,Cannot be wielded by those unworthy";
 	
     /**
      * Test the Equipment constructor and the getters
@@ -14,7 +14,7 @@ public class EquipmentTest
     public void constructorTest() throws AssertException
     {
         // Use the default constructor
-        Equipment eq = new Equipment(info);
+        Equipment eq = new Equipment(INFO);
 
         // The name should be "Thor's Hammer", description "Cannot be wielded by those unworthy",
         // totalWeight 10, totalPrice 99999999, and count 1.
@@ -26,58 +26,68 @@ public class EquipmentTest
     }
     
     /**
-     * tests the getColor method individually
-     * @throws AssertException
-     */
-    public void getColorTest() throws AssertException
-    {
-        // Use the full constructor
-        Animal animal = new Animal("green", "Kermit", 4.0, 20.0);
-
-        // The color should be "green"
-        Assert.assertEquals("green", animal.getColor());
-    }
-    
-    /**
      * tests the getName method individually
      * @throws AssertException
      */
     public void getNameTest() throws AssertException
     {
-        // Use the full constructor
-        Animal animal = new Animal("green", "Kermit", 4.0, 20.0);
+        // construct new object
+        Equipment eq = new Equipment(INFO);
 
-        // The name should be "Kermit"
-        Assert.assertEquals("Kermit", animal.getName());
+        // The name should be "Thor's Hammer"
+        Assert.assertEquals("Thor's Hammer", eq.getName());
     }
     
     /**
-     * tests the getWeight method individually
+     * tests the getDescription method individually
      * @throws AssertException
      */
-    public void getWeightTest() throws AssertException
+    public void getDescriptionTest() throws AssertException
     {
-        // Use the full constructor
-        Animal animal = new Animal("green", "Kermit", 4.0, 20.0);
+    	// construct new object
+        Equipment eq = new Equipment(INFO);
 
-        // The weight should be 4
-        Assert.assertEquals(20, animal.getWeight(), 0.01);
+        // The description should be "Cannot be wielded by those unworthy"
+        Assert.assertEquals("Cannot be wielded by those unworthy", eq.getDescription());
+    }
+    
+    /**
+     * tests the getCount method individually
+     * @throws AssertException
+     */
+    public void getCountTest() throws AssertException
+    {
+    	// construct new object
+    	Equipment eq = new Equipment(INFO);
+
+        // The count should be 1
+        Assert.assertEquals(1, eq.getCount(), 0.01);
     }
     
     /** 
-     * tests the getHeight method individually
+     * tests the getTotalWeight method individually
      * @throws AssertException
      */
-    public void getHeightTest() throws AssertException
+    public void getTotalWeightTest() throws AssertException
     {
-        // Use the full constructor
-        Animal animal = new Animal("green", "Kermit", 4.0, 20.0);
+    	// construct new object
+    	Equipment eq = new Equipment(INFO);
 
-        // The height should be 20
-        Assert.assertEquals("green", animal.getColor());
-        Assert.assertEquals("Kermit", animal.getName());
-        Assert.assertEquals(4, animal.getHeight(), 0.01);
-        Assert.assertEquals(20, animal.getWeight(), 0.01);
+        // The total weight should be 10
+        Assert.assertEquals(10, eq.getTotalWeight(), 0.01);
+    }
+    
+    /**
+     * tests the getTotalPrice method individually
+     * @throws AssertException
+     */
+    public void getTotalPriceTest() throws AssertException
+    {
+    	// construct new object
+    	Equipment eq = new Equipment(INFO);
+
+        // The total price should be 99999999
+        Assert.assertEquals(99999999, eq.getTotalPrice(), 0.01);
     }
     
     /**
@@ -86,11 +96,11 @@ public class EquipmentTest
      */
     public void toStringTest() throws AssertException
     {
-        // Use the full constructor
-        Animal animal = new Animal("green", "Kermit", 4.0, 20.0);
+    	// construct new object
+    	Equipment eq = new Equipment(INFO);
 
         // The toString should result in "Kermit, a green-colored animal. 20 pounds, 4 inches.\n"
-        Assert.assertEquals("Kermit, a green-colored animal. 20 pounds, 4 inches.\n", animal.toString());
+        Assert.assertEquals("Kermit, a green-colored animal. 20 pounds, 4 inches.\n", eq.toString());
     }
 }
 
