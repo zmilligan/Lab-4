4fa1c15090bef12dabb03549f4fc156110c94470
Zandi Milligan <zmilligan@ou.edu>
2019-02-09 10:23:18 -0600

    Completed Todo list

--- Lab 4/src/Todo.java
+++ Lab 4/src/Todo.java
@@ -1,6 +1,6 @@
 public class Todo {/*
 Lab Section_________: 12
-Last Modified_______: 2019-02-07
+Last Modified_______: 2019-02-09
 OU ID_______________: 112982309
 OU EMAIL____________: zmilligan@ou.edu
 
@@ -13,9 +13,10 @@ Tasks:
 6. ETest constructor test
 7. ETest getter tests
 8. ETest toString test
-9. 
-10. 
-11. 
+9. ZTest basic outline
+10. ZTest totalHeight test
+11. ZTest capacity test
+12. ZTest toString test
 
 Estimated Time (minutes):
 1. 7
@@ -26,9 +27,10 @@ Estimated Time (minutes):
 6. 5
 7. 8
 8. 6
-9. 
-10. 
-11. 
+9. 10
+10. 5
+11. 3
+12. 5
 
 Actual Time (minutes):
 1. 4
@@ -39,7 +41,8 @@ Actual Time (minutes):
 6. 6
 7. 10
 8. 5
-9. 
-10. 
-11. 
+9. 12
+10. 4
+11. 2
+12. 6
 */}
